2c677c4141240703105bc16f63dbe2589bc90f8d
Jayesh-Jain31 <jayeshjain66192@gmail.com>
2026-04-09 15:28:49 +0530

    Update SummaryApi.js

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://saree-shops.onrender.com";
+export const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const SummaryApi = {
     register : { url : '/api/user/register', method : 'post' },
